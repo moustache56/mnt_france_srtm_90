ca36e80f82d7dd312c855aefecb12f54d9353b51
eric-pommereau <eric.pommereau@gmail.com>
2016-02-21 21:52:20 +0100

    téléchargement srtm

--- vagrant-provision/bootstrap.sh
+++ vagrant-provision/bootstrap.sh
@@ -16,8 +16,8 @@ PG_HBA=/etc/postgresql/9.4/main/pg_hba.conf
 PG_CONF=/etc/postgresql/9.4/main/postgresql.conf
 RASTER_DIR=/vagrant/rasters/
 
-# Local solution to avoid downloading on the NET  (70 Mo each tile)
-RASTER_SERVER=http://mafreebox.freebox.fr/share/bR4v-TAcEYjBEqwj/
+Local solution to avoid downloading on the NET  (70 Mo each tile)
+#RASTER_SERVER=http://mafreebox.freebox.fr/share/bR4v-TAcEYjBEqwj/
 #RASTER_SERVER=http://srtm.csi.cgiar.org/SRT-ZIP/SRTM_V41/SRTM_Data_GeoTiff/
 
 # allow external connexions
